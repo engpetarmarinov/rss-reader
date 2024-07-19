0e2e3e9723aee56dad7fefa836877b000fa7151b
Petar Marinov <eng.petar.marinov@gmail.com>
2024-07-19 14:18:35 +0300

    sanitize models data save

--- app/app/models/feed.rb
+++ app/app/models/feed.rb
@@ -1,4 +1,11 @@
 class Feed < ApplicationRecord
   validates :name, presence: true
   validates :url, presence: true
+  before_save :sanitize_attributes
+
+  def sanitize_attributes
+    self.name = ActionController::Base.helpers.sanitize(self.name, tags: [])
+    self.url = ActionController::Base.helpers.sanitize(self.url, tags: [])
+    self.image = ActionController::Base.helpers.sanitize(self.image, tags: [])
+  end
 end
--- app/app/models/post.rb
+++ app/app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  before_save :sanitize_attributes
+
+  def sanitize_attributes
+    self.title = ActionController::Base.helpers.sanitize(self.title, tags: [])
+    self.link = ActionController::Base.helpers.sanitize(self.link, tags: [])
+    self.source = ActionController::Base.helpers.sanitize(self.source, tags: [])
+    self.source_url = ActionController::Base.helpers.sanitize(self.source_url, tags: [])
+    self.description = ActionController::Base.helpers.sanitize(self.description, tags: [])
+  end
 end
